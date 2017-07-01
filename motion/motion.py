c61440efee08d1489ddb851a0f020edfab784bad
tomoki sato <tomoki_sato@informorph.com>
2017-07-01 21:22:58 +0900

    Fix test is ok interpolation class

--- motion/motion.py
+++ motion/motion.py
@@ -6,27 +6,75 @@
 #
 
 
-class Interpolation:
+class Interpolation(object):
     """ waypoint間を補完するクラス """
-    
+
+
     def __init__(self):
         self.name = ""
         self.waypoints = []
         self.now_value = 0.0
-        self.parameter = 0.0
-    
+        self.now_target = 0.0
+        self.param_step = 0.0
+        self.step = 0.0
+        self.count = 0
+
+
     def __str__(self):
-        return "this is interpolation class."   # test
-    
+        return u'[Ip][waypoints, value, target, step]=%s' % [str(s) for s in self.waypoints, self.now_value, self.now_target, self.step]
+
+
+    def _calculate(self):
+        if abs(self.now_target - self.now_value) < abs(self.step):
+            self.now_value = self.now_target
+            try:
+                self.now_target = self.waypoints.pop(0)
+            except:
+                print(u'[Ip]Cannot pop next value from waypoints list')
+                return False
+        elif self.now_target > self.now_value:
+            self.now_value += self.step
+        elif self.now_target < self.now_value:
+            self.now_value -= self.step
+        else:
+            pass
+
+
     def set_waypoint(self, wp):
         self.waypoints.append(wp)
-    
+
+
     def get_waypoint(self):
         return self.waypoints
-    
+
+
+    def get_value(self):
+        return self.now_value
+
+
+    def get_target(self):
+        return self.now_target
+
+
+    def set_step(self, st):
+        self.step = st
+
+
     def next(self):
-        # calculate next step
+        """ calculate next step """
+        if self.count == 0:
+            try:
+                self.now_target = self.waypoints.pop(0)
+            except:
+                pass
+        else:
+            pass
+        
+        if self._calculate() is False:
+            self.count += 1
+            return False
+        else:
+            self.count += 1
+            return self.now_value
 
-    
-
-    
+        
--- test_motion.py
+++ test_motion.py
@@ -8,12 +8,26 @@
 import sys
 
 from motion.motion import Interpolation
-
-
-def main():
+def test_interpolation():
     ip = Interpolation()
     print(ip)
+    
+    test_waypoint = [10, 20, 30, -10, -20, 50]
+    for v in test_waypoint:
+        ip.set_waypoint(v)
+    print(ip)
+
+    test_step = 0.1
+    ip.set_step(test_step)
+    print(ip)
+
+    while ip.next() is not False:
+        print(ip)
+    
+    print('test_interpolation finished!')
+
+
 
 
 if __name__ == '__main__':
-    main()
+    test_interpolation()
