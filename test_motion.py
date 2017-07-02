20153d0aaccbd27522711c067ead031721257782
tomoki sato <tomoki_sato@balmuda.com>
2017-07-02 10:31:49 +0900

    Add robot class, test is successful

--- test_motion.py
+++ test_motion.py
@@ -5,9 +5,12 @@
 # tomoki_sato | 20170630
 #
 
-import sys
+import time
 
 from motion.motion import Interpolation
+from motion.robot import Robot
+
+
 def test_interpolation():
     ip = Interpolation()
     print(ip)
@@ -28,6 +31,28 @@ def test_interpolation():
 
 
 
+def test_robot():
+    r = Robot()
+    print(r)
+
+    test_pose_list = [  [10,20,30],
+                        [-10,20, 4],
+                        [0,3,-6],
+                        [3,3,3]]
+    r.set_pose_list(test_pose_list)
+    print(r)
+
+    test_step_list = [0.1, 0.2, 0.3, 1.0]
+    r.set_step(test_step_list)
+    print(r)
+
+    while r.next() != [False, False, False, False]:
+        time.sleep(0.05)    # for debug
+        print(r)
+
+
+
 
 if __name__ == '__main__':
     test_interpolation()
+    test_robot()
